1d8477d0d470ecd5b0c184b0c230b71ab269ecab
Patrick Shobe <patrick@patrickshobe.com>
2018-11-09 16:44:29 -0700

    Adds First encoder test and assets

---
+++ spec/services/encoder_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Video Encoder Service' do
+  it 'should encode a video' do
+
+    # Set up files for testing
+    dest_folder = 'fixtures/video_files/tmp'
+    dest_file_path = 'fixtures/video_files/tmp/star_trails.mkv'
+    test_file_path = 'fixtures/video_files/star_trails.mkv'
+    video_path = 'fixtures/video_files/tmp/star_trails.mkv'
+    FileUtils.cp(test_file_path, dest_folder)
+    encoded_path = 'fixtures/video_files/tmp/star_trails.mp4'
+
+    video  = Video.new( path: dest_file_path, )
+    Encoder.encode(video)
+
+    expect(File.file?(video_path)).to eq(false)
+    expect(File.file?(encoded_path)).to eq(true)
+    expect(File.extname(encoded_path)).to eq('.mp4')
+
+    # Clean Up
+    File.delete(encoded_path)
+  end
+end
